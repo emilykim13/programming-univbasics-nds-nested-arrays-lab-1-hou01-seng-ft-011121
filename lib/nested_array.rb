6e73426ae71b9d930bea91b1223cceeb0c2d0cb1
Emily Nguyen <emilyn0614@gmail.com>
2021-01-04 05:21:47 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -43,7 +43,7 @@ def matrix_lookup(matrix, row, column)
 
 assembled_matrix = [CONVENTIONAL_PRODUCE, ORGANIC_PRODUCE]
 sorted_matrix = [CONVENTIONAL_PRODUCE.sort, ORGANIC_PRODUCE.sort]
-
+matrix_lookup(assembled_matrix, 1, 1)
 end
 
 def matrix_update(matrix, row, column, new_value)
